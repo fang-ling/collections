879f4174e32c77ce3e12c646b74393f60030c73a
Fang Ling <alice@jxx.world>
2023-02-23 08:51:37 +0800

    (RedBlackTree) add public API

--- include/red_black_tree.h
+++ include/red_black_tree.h
@@ -41,4 +41,54 @@ struct RedBlackTree {
     Int (*compare)(void* lhs, void* rhs);
 };
 
+/** Begin: Creating a tree **/
+/* Creates a new, empty tree. */
+struct RedBlackTree*
+red_black_tree_init(Int element_size,
+                    Int (*compare)(void* lhs, void* rhs));
+/** End: Creating a tree **/
+
+/** Begin: Destroying a tree **/
+void red_black_tree_deinit(struct RedBlackTree* tree);
+/** End: Destroying a tree **/
+
+/** Begin: Insertion **/
+/* Insert a new item into the tree */
+void red_black_tree_insert(struct RedBlackTree* tree,
+                           void* key,
+                           Bool allow_duplicate_key);
+/** End: Insertion **/
+
+/** Begin: Removal **/
+// (Returns true if key is in the tree.)
+Bool red_black_tree_remove(struct RedBlackTree* tree, void* key);
+/** End: Removal **/
+
+/** Begin: Lookup **/
+/* Returns the smallest element in a search tree in O(lg n) time. */
+void* red_black_tree_min(struct RedBlackTree* tree);
+
+/* Returns the largest element in a search tree in O(lg n) time. */
+void* red_black_tree_max(struct RedBlackTree* tree);
+
+/* Returns the smallest key greater than the given key. */
+void* red_black_tree_successor(struct RedBlackTree* tree, void* key);
+
+/* Returns the largest key smaller than the given key. */
+void* red_black_tree_predecessor(struct RedBlackTree* tree, void* key);
+
+/* Returns the position of x with key in the linear order determined by an
+ * inorder tree walk of tree.
+ */
+Int red_black_tree_rank(struct RedBlackTree* tree, void* key);
+
+/* Returns the i-th smallest key in a tree */
+void* red_black_tree_select(struct RedBlackTree* tree, Int i);
+
+/* Returns a Boolean value indicating whether the tree contains the given
+ * element.
+ */
+Bool red_black_tree_contains(struct RedBlackTree* tree, void* key);
+/** End: Lookup **/
+
 #endif /* red_black_tree_h */
