7d30696d4a59bc022c7f1299ee71f959b93034c9
Fang Ling <alice@jxx.world>
2023-03-13 10:19:35 +0800

    (benchmark-lib) add swift collections

--- benchmark/Package.swift
+++ benchmark/Package.swift
@@ -8,7 +8,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-collections-benchmark",
                  from: "0.0.3"),
         .package(url: "https://github.com/fang-ling/swift-c-collections",
-                 from: "0.0.10")
+                 from: "0.0.10"),
+        .package(url: "https://github.com/apple/swift-collections",
+                 branch: "release/1.1")
     ],
     targets: [
         .executableTarget(
@@ -16,7 +18,8 @@ let package = Package(
             dependencies: [
                 .product(name: "CollectionsBenchmark",
                          package: "swift-collections-benchmark"),
-                .product(name: "CCollections", package: "swift-c-collections")
+                .product(name: "CCollections", package: "swift-c-collections"),
+                .product(name: "Collections", package: "swift-collections")
             ]),
         .testTarget(
             name: "benchmarkTests",
