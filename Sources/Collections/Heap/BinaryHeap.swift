044e337fe69ee865f40c060236312b24c7f5792a
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-09-22 12:35:48 +0800

    Add O(n) initializer for binary heap

--- Sources/Collections/Heap/BinaryHeap.swift
+++ Sources/Collections/Heap/BinaryHeap.swift
@@ -11,28 +11,55 @@ import Foundation
 //                         Structure Definition                               //
 //----------------------------------------------------------------------------//
 public struct BinaryHeap<Element : Comparable> {
-    @usableFromInline
-    var _storage : [Element]
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    //                     Inspection of a BinaryHeap                         //
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    /// The number of elements in the binary heap.
-    @inlinable
-    public var count : Int { return _storage.count }
-
-    /// A Boolean value indicating whether the binary heap is empty.
-    @inlinable
-    public var is_empty : Bool { return _storage.isEmpty }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    //                     Creation of a BinaryHeap                           //
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    /// Creates a new, empty heap.
-    @inlinable
-    public init() {
-        _storage = [Element]()
+  @usableFromInline
+  var _storage : [Element]
+  
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+  //                     Inspection of a BinaryHeap                         //
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+  /// The number of elements in the binary heap.
+  @inlinable
+  public var count : Int { return _storage.count }
+  
+  /// A Boolean value indicating whether the binary heap is empty.
+  @inlinable
+  public var is_empty : Bool { return _storage.isEmpty }
+  
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+  //                     Creation of a BinaryHeap                           //
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+  /// Creates a new, empty heap.
+  @inlinable
+  public init() {
+    _storage = [Element]()
+  }
+  
+  /// Initializes a heap from a sequence.
+  /// - Complexity: O(n), where n is the number of items in `elements`.
+  @inlinable
+  public init<S : Sequence>(_ elements : S) where S.Element == Element {
+    _storage = Array(elements)
+    guard count > 1 else { return }
+    
+    for i in stride(from: count / 2 - 1, through: 0, by: -1) {
+      max_heapify_down(i)
     }
+  }
+}
+
+extension BinaryHeap: ExpressibleByArrayLiteral {
+  /// Creates a new heap from the contents of an array literal.
+  ///
+  /// **Do not call this initializer directly.** It is used by the compiler when
+  /// you use an array literal. Instead, create a new heap using an array
+  /// literal as its value by enclosing a comma-separated list of values in
+  /// square brackets. You can use an array literal anywhere a heap is expected
+  /// by the type context.
+  ///
+  /// - Parameter elements: A variadic list of elements of the new heap.
+  public init(arrayLiteral elements: Element...) {
+    self.init(elements)
+  }
 }
 
 //----------------------------------------------------------------------------//
@@ -52,89 +79,97 @@ func bh_right(_ i : Int) -> Int { return 2 * i + 2 }
 //                         Maintenance of the BinaryHeap property             //
 //----------------------------------------------------------------------------//
 extension BinaryHeap {
-    ///Maintain the max heap property from node i all the way up to root.
-    @inlinable
-    mutating func max_heapify_up(_ i : Int) {
-        if i == 0 { /* We are at root node now. */
-            return
-        }
-        if _storage[bh_parent(i)] <= _storage[i] {
-            _storage.swapAt(bh_parent(i), i) /* Exchange i with parent(i) */
-            /*
-             * Current node is satisfy max heap property, but maybe not its
-             * parent. Fix this recursively.
-             */
-            max_heapify_up(bh_parent(i))
-        }
+  ///Maintain the max heap property from node i all the way up to root.
+  @inlinable
+  mutating func max_heapify_up(_ i : Int) {
+    if i == 0 { /* We are at root node now. */
+      return
     }
-
-    ///Maintain the max heap property from node i all the way down to leaf.
-    @inlinable
-    mutating func max_heapify_down(_ i : Int) {
-        if i >= count / 2 { /* No children, we are done */
-            return
-        }
-        var j = 0
-        /* Find i's largest children j. */
-        if bh_left(i) < count && bh_right(i) < count { /* Both children exist */
-            j =
-              _storage[bh_left(i)] > _storage[bh_right(i)] ?
-              bh_left(i) :
-              bh_right(i)
-        } else if bh_left(i) < count && bh_right(i) >= count { /* Left only */
-            j = bh_left(i)
-        } else if bh_left(i) >= count && bh_right(i) < count { /* Right only */
-            j = bh_right(i)
-        } else { /* Will never happen */
-            print("error")
-            return
-        }
-        /*
-         * If i is smaller than j (i's largest child), we are violate the max
-         * heap property. Fix it by swap i.key with j.key and then recur on j.
-         */
-        if _storage[i] < _storage[j] {
-            _storage.swapAt(i, j)
-            max_heapify_down(j)
-        }
+    if _storage[bh_parent(i)] <= _storage[i] {
+      _storage.swapAt(bh_parent(i), i) /* Exchange i with parent(i) */
+      /*
+       * Current node is satisfy max heap property, but maybe not its
+       * parent. Fix this recursively.
+       */
+      max_heapify_up(bh_parent(i))
     }
+  }
+  
+  ///Maintain the max heap property from node i all the way down to leaf.
+  @inlinable
+  mutating func max_heapify_down(_ i : Int) {
+    if i >= count / 2 { /* No children, we are done */
+      return
+    }
+    var j = 0
+    /* Find i's largest children j. */
+    if bh_left(i) < count && bh_right(i) < count { /* Both children exist */
+      j =
+      _storage[bh_left(i)] > _storage[bh_right(i)] ?
+      bh_left(i) :
+      bh_right(i)
+    } else if bh_left(i) < count && bh_right(i) >= count { /* Left only */
+      j = bh_left(i)
+    } else if bh_left(i) >= count && bh_right(i) < count { /* Right only */
+      j = bh_right(i)
+    } else { /* Will never happen */
+      print("error")
+      return
+    }
+    /*
+     * If i is smaller than j (i's largest child), we are violate the max
+     * heap property. Fix it by swap i.key with j.key and then recur on j.
+     */
+    if _storage[i] < _storage[j] {
+      _storage.swapAt(i, j)
+      max_heapify_down(j)
+    }
+  }
 }
 
 //----------------------------------------------------------------------------//
 //                         Lookup                                             //
 //----------------------------------------------------------------------------//
 extension BinaryHeap {
-    /// Returns the largest element in a heap in constant time.
-    @inlinable
-    public var max : Element? {
-        return _storage.first
-    }
+  /// Returns the largest element in a heap in constant time.
+  @inlinable
+  public var max : Element? {
+    return _storage.first
+  }
 }
 
 //----------------------------------------------------------------------------//
 //                         Insertion                                          //
 //----------------------------------------------------------------------------//
 extension BinaryHeap {
-    /// Inserts a new item into heap.
-    @inlinable
-    public mutating func insert(_ new_element : Element) {
-        _storage.append(new_element)
-        max_heapify_up(count - 1)
-    }
+  /// Inserts a new item into heap.
+  /// - Complexity: O(lg n), where n is the length of the heap
+  @inlinable
+  public mutating func insert(_ new_element : Element) {
+    _storage.append(new_element)
+    max_heapify_up(count - 1)
+  }
 }
 
 //----------------------------------------------------------------------------//
 //                         Removal                                            //
 //----------------------------------------------------------------------------//
 extension BinaryHeap {
-    /// Removes the max item in the heap.
-    @inlinable
-    @discardableResult
-    public mutating func remove_max() -> Element {
-        _storage.swapAt(0, count - 1)
-        /* It's necessary to first do remove to update `count` */
-        let r = _storage.removeLast()
-        max_heapify_down(0)
-        return r
-    }
+  /// Removes the max item in the heap.
+  @inlinable
+  @discardableResult
+  public mutating func remove_max() -> Element {
+    _storage.swapAt(0, count - 1)
+    /* It's necessary to first do remove to update `count` */
+    let r = _storage.removeLast()
+    max_heapify_down(0)
+    return r
+  }
+}
+
+//----------------------------------------------------------------------------//
+//                        Accessing Elements                                  //
+//----------------------------------------------------------------------------//
+extension BinaryHeap {
+  
 }
--- Tests/CollectionsTests/HeapTests/BinaryHeapTests.swift
+++ Tests/CollectionsTests/HeapTests/BinaryHeapTests.swift
@@ -9,143 +9,154 @@ import XCTest
 @testable import Collections
 
 final class BinaryHeapTests : XCTestCase {
-    func test_is_empty() {
-        var heap = BinaryHeap<Int>()
-        XCTAssertTrue(heap.is_empty)
-
-        heap.insert(19358)
-        XCTAssertFalse(heap.is_empty)
-
-        heap.remove_max()
-        XCTAssertTrue(heap.is_empty)
+  func test_is_empty() {
+    var heap = BinaryHeap<Int>()
+    XCTAssertTrue(heap.is_empty)
+    
+    heap.insert(19358)
+    XCTAssertFalse(heap.is_empty)
+    
+    heap.remove_max()
+    XCTAssertTrue(heap.is_empty)
+  }
+  
+  func test_count() {
+    var heap = BinaryHeap<Int>()
+    XCTAssertEqual(heap.count, 0)
+    
+    heap.insert(19358)
+    XCTAssertEqual(heap.count, 1)
+    
+    heap.insert(12321)
+    XCTAssertEqual(heap.count, 2)
+    
+    heap.remove_max()
+    XCTAssertEqual(heap.count, 1)
+  }
+  
+  struct _Task : Comparable & Equatable {
+    var name : String
+    var priority : Int
+    
+    static func < (lhs : _Task, rhs : _Task) -> Bool {
+      return lhs.priority < rhs.priority
     }
-
-    func test_count() {
-        var heap = BinaryHeap<Int>()
-        XCTAssertEqual(heap.count, 0)
-
-        heap.insert(19358)
-        XCTAssertEqual(heap.count, 1)
-
-        heap.insert(12321)
-        XCTAssertEqual(heap.count, 2)
-
-        heap.remove_max()
-        XCTAssertEqual(heap.count, 1)
+  }
+  
+  func test_insert() {
+    var heap = BinaryHeap<_Task>()
+    
+    XCTAssertEqual(heap.count, 0)
+    heap.insert(_Task(name: "Ssu-yen", priority: 12333))
+    XCTAssertEqual(heap.count, 1)
+  }
+  
+  func test_max() {
+    var heap = BinaryHeap<Int>()
+    XCTAssertNil(heap.max)
+    
+    heap.insert(12333)
+    XCTAssertEqual(heap.max, 12333)
+    
+    heap.insert(12321)
+    XCTAssertEqual(heap.max, 12333)
+    
+    heap.insert(19348)
+    XCTAssertEqual(heap.max, 19348)
+    
+    heap.insert(19358)
+    XCTAssertEqual(heap.max, 19358)
+  }
+  
+  func test_remove() {
+    var heap = BinaryHeap<Int>()
+    
+    heap.insert(19358)
+    XCTAssertEqual(heap.remove_max(), 19358)
+    
+    heap.insert(12)
+    heap.insert(9)
+    XCTAssertEqual(heap.remove_max(), 12)
+    
+    heap.insert(13)
+    heap.insert(1)
+    heap.insert(4)
+    XCTAssertEqual(heap.remove_max(), 13)
+    
+    let input = Array(1 ... 20).shuffled()
+    for i in input {
+      heap.insert(i)
     }
-
-    struct _Task : Comparable & Equatable {
-        var name : String
-        var priority : Int
-
-        static func < (lhs : _Task, rhs : _Task) -> Bool {
-            return lhs.priority < rhs.priority
-        }
-    }
-
-    func test_insert() {
-        var heap = BinaryHeap<_Task>()
-
-        XCTAssertEqual(heap.count, 0)
-        heap.insert(_Task(name: "Ssu-yen", priority: 12333))
-        XCTAssertEqual(heap.count, 1)
-    }
-
-    func test_max() {
-        var heap = BinaryHeap<Int>()
-        XCTAssertNil(heap.max)
-
-        heap.insert(12333)
-        XCTAssertEqual(heap.max, 12333)
-
-        heap.insert(12321)
-        XCTAssertEqual(heap.max, 12333)
-
-        heap.insert(19348)
-        XCTAssertEqual(heap.max, 19348)
-
-        heap.insert(19358)
-        XCTAssertEqual(heap.max, 19358)
+    print(heap.count)
+    
+    XCTAssertEqual(heap.remove_max(), 20)
+    XCTAssertEqual(heap.remove_max(), 19)
+    XCTAssertEqual(heap.remove_max(), 18)
+    XCTAssertEqual(heap.remove_max(), 17)
+    XCTAssertEqual(heap.remove_max(), 16)
+    XCTAssertEqual(heap.remove_max(), 15)
+    XCTAssertEqual(heap.remove_max(), 14)
+    XCTAssertEqual(heap.remove_max(), 13)
+    XCTAssertEqual(heap.remove_max(), 12)
+    XCTAssertEqual(heap.remove_max(), 11)
+    XCTAssertEqual(heap.remove_max(), 10)
+    XCTAssertEqual(heap.remove_max(), 9)
+    /* One 9 was still in the heap from before */
+    XCTAssertEqual(heap.remove_max(), 9)
+    XCTAssertEqual(heap.remove_max(), 8)
+    XCTAssertEqual(heap.remove_max(), 7)
+    XCTAssertEqual(heap.remove_max(), 6)
+    XCTAssertEqual(heap.remove_max(), 5)
+    XCTAssertEqual(heap.remove_max(), 4)
+    /* One 4 was still in the heap from before */
+    XCTAssertEqual(heap.remove_max(), 4)
+    XCTAssertEqual(heap.remove_max(), 3)
+    XCTAssertEqual(heap.remove_max(), 2)
+    XCTAssertEqual(heap.remove_max(), 1)
+    /* One 1 was still in the heap from before */
+    XCTAssertEqual(heap.remove_max(), 1)
+    
+    XCTAssertEqual(heap.count, 0)
+  }
+  
+  func test_max_struct() {
+    var heap = BinaryHeap<_Task>()
+    
+    let alpha = _Task(name: "Ssu-yen", priority: 12333)
+    heap.insert(alpha)
+    XCTAssertEqual(alpha, heap.max)
+    
+    heap.insert(_Task(name: "Tzu-han", priority: 1))
+    XCTAssertEqual(alpha, heap.max)
+    
+    let gamma = _Task(name: "Ya-hsüan", priority: 19342)
+    heap.insert(gamma)
+    XCTAssertEqual(gamma, heap.max)
+  }
+  
+  func test_remove_struct() {
+    var heap = BinaryHeap<_Task>()
+    
+    let alpha = _Task(name: "Ssu-yen", priority: 12333)
+    let beta = _Task(name: "Tzu-han", priority: 1)
+    let gamma = _Task(name: "Ya-hsüan", priority: 19342)
+    
+    heap.insert(alpha)
+    heap.insert(beta)
+    heap.insert(gamma)
+    XCTAssertEqual(heap.remove_max(), gamma)
+    XCTAssertEqual(heap.remove_max(), alpha)
+    XCTAssertEqual(heap.remove_max(), beta)
+  }
+  
+  func test_init_array() {
+    var heap : BinaryHeap = [23412, 32941, 3491391, 3245, 342423]
+    for i in [23412, 32941, 3491391, 3245, 342423].sorted().reversed() {
+      XCTAssertEqual(heap.remove_max(), i)
     }
     
-    func test_remove() {
-        var heap = BinaryHeap<Int>()
-
-        heap.insert(19358)
-        XCTAssertEqual(heap.remove_max(), 19358)
-
-        heap.insert(12)
-        heap.insert(9)
-        XCTAssertEqual(heap.remove_max(), 12)
-
-        heap.insert(13)
-        heap.insert(1)
-        heap.insert(4)
-        XCTAssertEqual(heap.remove_max(), 13)
-        
-        let input = Array(1 ... 20).shuffled()
-        for i in input {
-            heap.insert(i)
-        }
-        print(heap.count)
-        
-        XCTAssertEqual(heap.remove_max(), 20)
-        XCTAssertEqual(heap.remove_max(), 19)
-        XCTAssertEqual(heap.remove_max(), 18)
-        XCTAssertEqual(heap.remove_max(), 17)
-        XCTAssertEqual(heap.remove_max(), 16)
-        XCTAssertEqual(heap.remove_max(), 15)
-        XCTAssertEqual(heap.remove_max(), 14)
-        XCTAssertEqual(heap.remove_max(), 13)
-        XCTAssertEqual(heap.remove_max(), 12)
-        XCTAssertEqual(heap.remove_max(), 11)
-        XCTAssertEqual(heap.remove_max(), 10)
-        XCTAssertEqual(heap.remove_max(), 9)
-        /* One 9 was still in the heap from before */
-        XCTAssertEqual(heap.remove_max(), 9)
-        XCTAssertEqual(heap.remove_max(), 8)
-        XCTAssertEqual(heap.remove_max(), 7)
-        XCTAssertEqual(heap.remove_max(), 6)
-        XCTAssertEqual(heap.remove_max(), 5)
-        XCTAssertEqual(heap.remove_max(), 4)
-        /* One 4 was still in the heap from before */
-        XCTAssertEqual(heap.remove_max(), 4)
-        XCTAssertEqual(heap.remove_max(), 3)
-        XCTAssertEqual(heap.remove_max(), 2)
-        XCTAssertEqual(heap.remove_max(), 1)
-        /* One 1 was still in the heap from before */
-        XCTAssertEqual(heap.remove_max(), 1)
-
-        XCTAssertEqual(heap.count, 0)
-    }
-
-    func test_max_struct() {
-        var heap = BinaryHeap<_Task>()
-
-        let alpha = _Task(name: "Ssu-yen", priority: 12333)
-        heap.insert(alpha)
-        XCTAssertEqual(alpha, heap.max)
-
-        heap.insert(_Task(name: "Tzu-han", priority: 1))
-        XCTAssertEqual(alpha, heap.max)
-
-        let gamma = _Task(name: "Ya-hsüan", priority: 19342)
-        heap.insert(gamma)
-        XCTAssertEqual(gamma, heap.max)
-    }
-
-    func test_remove_struct() {
-        var heap = BinaryHeap<_Task>()
-
-        let alpha = _Task(name: "Ssu-yen", priority: 12333)
-        let beta = _Task(name: "Tzu-han", priority: 1)
-        let gamma = _Task(name: "Ya-hsüan", priority: 19342)
-
-        heap.insert(alpha)
-        heap.insert(beta)
-        heap.insert(gamma)
-        XCTAssertEqual(heap.remove_max(), gamma)
-        XCTAssertEqual(heap.remove_max(), alpha)
-        XCTAssertEqual(heap.remove_max(), beta)
-    }
+    heap = [19358]
+    XCTAssertEqual(heap.remove_max(), 19358)
+    XCTAssertEqual(heap.count, 0)
+  }
 }
